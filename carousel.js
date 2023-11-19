d0a608c90e60ef089c2f4e1ac0583259df0fd145
neener <janinemrosen@gmail.com>
2023-11-19 18:10:27 -0500

    add currently focused carousel navigation

--- carousel.js
+++ carousel.js
@@ -12,7 +12,9 @@ document.addEventListener('DOMContentLoaded', function () {
         swipeAngle: false,
         speed: 400,
         nav: true,
+        navPosition: 'bottom',
         arrowKeys: true,
+        preventActionWhenRunning: true,
         autoHeight: true,
         controlsContainer: document.getElementById('tns-controls' + (index + 1)), // Use the controls container
         onInit: function (info, eventName) {
@@ -20,10 +22,39 @@ document.addEventListener('DOMContentLoaded', function () {
           slider.refresh();
         },
       });
-      
+
       // Add unique ID to each slider to handle multiple sliders
       var sliderId = 'slider' + (index + 1);
       carousel.setAttribute('data-slider-id', sliderId);
     });
+  });
+
+  // Add event listener for arrow keys
+  document.addEventListener('keydown', function (e) {
+    // Check if arrow key is pressed
+    if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+      // Find the carousel in focus
+      var focusedCarousel = document.querySelector('.carousel-container:focus');
+
+      // Check if there is a focused carousel
+      if (focusedCarousel) {
+        // Find the corresponding slider instance
+        var sliderId = focusedCarousel.getAttribute('data-slider-id');
+        var slider = tns.instances[sliderId];
+
+        // Check if the slider instance exists
+        if (slider) {
+          // Prevent default arrow key behavior
+          e.preventDefault();
+
+          // Navigate the slider based on the arrow key pressed
+          if (e.key === 'ArrowLeft') {
+            slider.goTo('prev');
+          } else if (e.key === 'ArrowRight') {
+            slider.goTo('next');
+          }
+        }
+      }
+    }
   });
 });

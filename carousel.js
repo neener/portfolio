bfa896d40123552bb165d0e648d208b5f0631926
neener <janinemrosen@gmail.com>
2023-11-25 12:07:11 -0500

    add lazyload and fullscreen from flickity

--- carousel.js
+++ carousel.js
@@ -1,13 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var containers = document.querySelectorAll('.container.main');
+// document.addEventListener('DOMContentLoaded', function () {
+//   var containers = document.querySelectorAll('.container.main');
 
-  containers.forEach(function (container) {
-    var carouselElement = container.querySelector('.carousel-cell');
-    new Flickity(carouselElement, {
-      cellAlign: 'left',
-      contain: true,
-      wrapAround: false,
-      fullscreen: true,
-    });
-  });
+//   containers.forEach(function (container) {
+//     var carouselElement = container.querySelector('.carousel-cell');
+//     new Flickity(carouselElement, {
+//       cellAlign: 'left',
+//       contain: true,
+//       wrapAround: false,
+//       fullscreen: true,
+//     });
+//   });
+// });
+var flkty = new Flickity('.carousel', {
+  cellAlign: 'left',
+  contain: true,
+  wrapAround: true, // Enable wrap-around behavior
+  fullscreen: true,
+  lazyLoad: 1
 });
